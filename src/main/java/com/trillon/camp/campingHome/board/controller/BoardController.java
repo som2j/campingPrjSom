297a14fc58f64f753f96a19a0993bb82337eaba5
gon-2 <95611330+gon-2@users.noreply.github.com>
2023-03-30 09:43:21 +0900

    [YG] 게시글 및 이미지 업로드 완료

--- src/main/java/com/trillon/camp/campingHome/board/controller/BoardController.java
+++ src/main/java/com/trillon/camp/campingHome/board/controller/BoardController.java
@@ -1,6 +1,26 @@
 package com.trillon.camp.campingHome.board.controller;
 import com.trillon.camp.campingHome.board.dto.BoardForm;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.trillon.camp.campingHome.board.dto.BoardForm;
 import com.trillon.camp.campingHome.board.dto.Reply;
 import com.trillon.camp.campingHome.board.service.BoardService;
 
